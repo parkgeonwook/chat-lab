fe0adf492359602d1c34bcbece7b8e1a321f4b09
parkgeonwook <rjsdnr6331@gmail.com>
2026-06-15 15:22:10 +0900

    first commit

---
+++ src/main/java/com/example/chatlap/ChatLapApplication.java
@@ -0,0 +1,13 @@
+package com.example.chatlap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatLapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatLapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/chatlap/ChatLapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chatlap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatLapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
