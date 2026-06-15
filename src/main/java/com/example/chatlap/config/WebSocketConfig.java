4f1645b896b2602f310e99807e93d18aab0708c0
parkgeonwook <rjsdnr6331@gmail.com>
2026-06-15 19:50:43 +0900

    feat: WebSocket 설정 추가

---
+++ src/main/java/com/example/chatlap/config/WebSocketConfig.java
@@ -0,0 +1,23 @@
+package com.example.chatlap.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws");
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.setApplicationDestinationPrefixes("/app"); // /app/...로 들어오는 메시지는 서버가 받음
+        registry.enableSimpleBroker("/queue"); // /queue/...로 보내는 메시지는 구독자에게 전달
+    }
+}
